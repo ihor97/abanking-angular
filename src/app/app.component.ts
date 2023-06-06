61d8895ba436669695de9dcd13141b6bd646866b
ihor97 <daniel.waffner@gmail.com>
2023-06-06 18:53:25 +0100

    Viewchild,Renderer2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Renderer2 } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'abanking-angular';
+export class AppComponent implements AfterViewInit {
+  // якщо ми звертаємось через ViewChild ми можемо получити доступ до силки тільки в afterViewInit
+  @ViewChild('input') public input:ElementRef
+  @ViewChild('button') public button:ElementRef
+  // сюжи навіть можна передати компоненту 
+  // доступ до компоненти 
+  @ViewChild(LayoutComponent) public set test(component:LayoutComponent){
+    component.say();
+  }
+
+
+  // кращий підхід через рендерер(застосовується в директивах)
+constructor(private renderer2:Renderer2){
+
+}
+
+  ngAfterViewInit(): void {
+  //  this.input.nativeElement.focus()
+  //   this.renderer2.addClass(this.input.nativeElement,'width')
+  //   const text:string=this.renderer2.createText('text')
+  //   // добавляємо наш текст
+  //   this.renderer2.appendChild(this.button.nativeElement,text)
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { Layout1Component } from './components/layout1/layout1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    Layout1Component
   ],
   imports: [
     BrowserModule
